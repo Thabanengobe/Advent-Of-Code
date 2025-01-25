7619a7733f63c34d0a427d6419aefbd33bbbddb9
Thabane Ngobe <93321759+Thabanengobe@users.noreply.github.com>
2025-01-25 15:32:01 +0200

    Add files via upload

--- Day0/Day.java
+++ Day0/Day.java
@@ -1,1 +1,19 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
+public class Day {
+  
+  public static List<String> readFiledata(String fileName){
+
+    try {
+      final Path filePath = Paths.get(fileName);
+      final List<String> file = Files.readAllLines(filePath);
+      return file;  
+
+    } catch (Exception e) {
+      return null;
+    }
+  }
+}
