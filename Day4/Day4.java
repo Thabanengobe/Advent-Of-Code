40d1b5bb9b0ee7fc065800464da919724ef219d6
Thabane Ngobe <93321759+Thabanengobe@users.noreply.github.com>
2025-01-28 23:14:36 +0200

    Removed Redunded code

--- Day4/Day4.java
+++ Day4/Day4.java
@@ -14,30 +14,6 @@ public class Day4  extends Day{
     return wordPuzzle;
   }
 
-  public static boolean findHorizontalXmasBack(int row, int col, String[][] puzzle){
-    if(((col+1)-4) >= 0){
-      String xmas = puzzle[row][col] + puzzle[row][col-1] + puzzle[row][col-2] + puzzle[row][col-3];
-      return xmas.equals("XMAS") | xmas.equals("SAMX");
-    }
-    return false;
-  }
-
-  public static boolean findDiagonalXmasback(int row, int col, String[][] puzzle){
-    if((((col+1)-4) >= 0) && (((row+1)-4) >= 0)){
-      String xmas = puzzle[row][col] + puzzle[row-1][col-1] + puzzle[row-2][col-2] + puzzle[row-3][col-3];
-      return xmas.equals("XMAS") | xmas.equals("SAMX");
-    }
-    return false;
-  }
-  
-  public static boolean findVerticalXmasBack(int row, int col, String[][] puzzle){
-    if(((row+1)-4) >= 0){
-      String xmas = puzzle[row][col] + puzzle[row-1][col] + puzzle[row-2][col] + puzzle[row-3][col];
-      return xmas.equals("XMAS") | xmas.equals("SAMX");
-    }
-    return false;
-  }
-
   public static boolean findHorizontalXmas(int row, int col, String[][] puzzle){
 
     if(puzzle.length - col >= 4){
@@ -64,17 +40,10 @@ public class Day4  extends Day{
     }
     return false;
   }
+
   public static boolean findDiagonalXmasX(int row, int col, String[][] puzzle){
     if(puzzle.length - row >= 4 && (col+1)-4 >= 0){
       String xmas = puzzle[row][col] + puzzle[row+1][col-1] + puzzle[row+2][col-2] + puzzle[row+3][col-3];
-      return xmas.equals("XMAS") | xmas.equals("SAMX");
-    }
-    return false;
-  }
-
-  public static boolean findDiagonalXmasY(int row, int col, String[][] puzzle){
-    if(puzzle.length - col >= 4 && (row+1)-4 >= 0){
-      String xmas = puzzle[row][col] + puzzle[row-1][col+1] + puzzle[row-2][col+2] + puzzle[row-3][col+3];
       return xmas.equals("XMAS") | xmas.equals("SAMX");
     }
     return false;
@@ -87,17 +56,12 @@ public class Day4  extends Day{
     for(int i =0; i < puzzle.length; i++){
       for(int j=0; j < puzzle.length; j++){
         if(findHorizontalXmas(i, j, puzzle)) xmas++;
-        if(findVerticalXmasBack(i, j, puzzle)) xmas++;
-        if(findHorizontalXmas(i, j, puzzle)) xmas++;
-        if(findVerticalXmasBack(i, j, puzzle)) xmas++;
+        if(findVerticalXmas(i, j, puzzle)) xmas++;
         if(findDiagonalXmas(i, j, puzzle)) xmas++;
-        if(findDiagonalXmasback(i, j, puzzle)) xmas++;
         if(findDiagonalXmasX(i, j, puzzle)) xmas++;
-        if(findDiagonalXmasY(i, j, puzzle)) xmas++;
-
       }
     }
-    System.out.println(xmas/2);
+    System.out.println(xmas);
   }
   
 }
